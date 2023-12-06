95bd8feb24d3e2785f53a491085f8eaa6eb2e3d8
Jeen <eyungenius@gmail.com>
2023-12-06 11:04:03 +0900

    project init & library added

---
+++ src/main/java/com/mingle/MingleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MingleSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mingle/MingleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
