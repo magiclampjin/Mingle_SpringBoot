9411dfe49dda19641dc03f9239ad26a7b73918f6
qwee0328 <qwee0328@naver.com>
2023-12-13 17:45:12 +0900

    이메일, 전화번호 중복검사

--- src/main/java/com/mingle/domain/repositories/MemberRepository.java
+++ src/main/java/com/mingle/domain/repositories/MemberRepository.java
@@ -6,16 +6,32 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.mingle.domain.entites.Member;
 
-public interface MemberRepository extends JpaRepository<Member, String>{
+public interface MemberRepository extends JpaRepository<Member, String> {
 	// 로그인한 사용자 nickName 불러오기
 	@Query("select m.nickname from Member m where m.id = :id")
 	String selectUserNickName(@Param("id") String id);
-	
+
 	// 아이디 중복검사
 	@Query("select count(*) from Member m where m.id=:id")
 	Long countById(@Param("id") String id);
 
 	default boolean idDuplicateCheck(String id) {
-	    return countById(id) > 0;
+		return countById(id) > 0;
+	}
+
+	// 이메일 중복검사
+	@Query("select count(*) from Member m where m.email=:email")
+	Long countByEmail(@Param("email") String email);
+
+	default boolean emailDuplicateCheck(String email) {
+		return countByEmail(email) > 0;
+	}
+
+	// 전화번호 중복검사
+	@Query("select count(*) from Member m where m.phone=:phone")
+	Long countByPhone(@Param("phone") String phone);
+
+	default boolean phoneDuplicateCheck(String phone) {
+		return countByPhone(phone) > 0;
 	}
 }
