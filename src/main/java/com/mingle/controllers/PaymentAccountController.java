9f1e0c60f4ecbc8cd15bcfd4a7f21c268747b604
HMK <cypher_1013@naver.com>
2023-12-18 17:33:45 +0900

    전화번호 수정

--- src/main/java/com/mingle/controllers/PaymentAccountController.java
+++ src/main/java/com/mingle/controllers/PaymentAccountController.java
@@ -63,10 +63,6 @@ public class PaymentAccountController {
 	// 등록된 계좌 수정하기
 	@PutMapping("/accountUpdate")
 	public ResponseEntity<Void> updateById(Authentication authentication, @RequestBody PaymentAccountDTO dto){
-		System.out.println(authentication.getName());
-		System.out.println(dto.getBankId());
-		System.out.println(dto.getAccountNumber());
-		
 		paServ.updateById(authentication.getName(),dto);
 		
 		return ResponseEntity.ok().build();
