e44e21886fdd5e5582c724da0d368a0a89a010e5
qwee0328 <qwee0328@naver.com>
2023-12-18 09:42:59 +0900

    로그인 실패하면 null값 반환하도록 변경

--- src/main/java/com/mingle/services/SecurityService.java
+++ src/main/java/com/mingle/services/SecurityService.java
@@ -19,8 +19,12 @@ public class SecurityService implements UserDetailsService{
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		System.out.println("loginID : "+username);
 		Member m = mRepo.findById(username).get();
-		SecurityUser user = new SecurityUser(m);
-		System.out.println(user.toString());
-		return user;
+		if(m!=null) {
+			SecurityUser user = new SecurityUser(m);
+			System.out.println(user.toString());
+			return user;
+		}else {
+			return null;
+		}
 	}
 }
